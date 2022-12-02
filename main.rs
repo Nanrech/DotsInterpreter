3cd6f18ee0607b9b4d52ba4dc8e82073f7daa4af
Nan R <88463477+Nanrech@users.noreply.github.com>
2022-12-02 08:29:39 +0100

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,6 +1,7 @@
 use std::fs::read_to_string;
 use std::collections::HashMap;
 
+
 fn main() {
     let file: String = read_to_string("src/main.dots")
         .expect("Couldn't read file.");
@@ -10,23 +11,37 @@ fn main() {
         .split(";\n")
         .collect();
 
+    // Bit ugy but idk
     let dots_eop: &str = ".;";
     let dots_print: &str = "..";
     let dots_var: &str = "...";
 
+    // All vars are global
     let mut dots_variables = HashMap::new();
 
-    assert!(tokens.ends_with(&[dots_eop]));
+    assert!(tokens[tokens.len() - 1].ends_with(dots_eop));
 
-    for token in tokens {
+    for t in tokens {
+        // Just dupe it so we can remove the leading dots
+        let mut token = String::from(t);
+
+        println!("{}", token);
+
         if token == dots_eop {
-            println!("y");
+            println!("Reached the end of the program.");
             return;
         }
         else if token.starts_with(dots_var) {
             // Tokenize: [...][name][value]
             // Everything else can be ignored so
             // we just don't look at whatever is after that
+
+            for _ in 0..=2 {
+                token.remove(0);
+            }
+
+            println!("{}", token);
+
             let split_token: Vec<&str> = token
                 .split(" ")
                 .collect();
@@ -41,14 +56,14 @@ fn main() {
         else if token.starts_with(dots_print) {
             // TODO: Slice token starting from ..!
             // Imma go with .name. for printing variables because why not
+
+            for _ in 0..=1 {
+                token.remove(0);
+            }
+
             let split_token: Vec<&str> = token
                 .split(" ")
                 .collect();
-            for c in split_token {
-                println!("{}", c);
-            }
         }
     }
-    // println!("{}", file);
-
 }
