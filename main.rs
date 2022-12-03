4b5dd64e55adff6ab2140f67c073f3407d2b7cfb
Nan R <88463477+Nanrech@users.noreply.github.com>
2022-12-03 12:10:53 +0100

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,69 +1,86 @@
 use std::fs::read_to_string;
 use std::collections::HashMap;
+use std::env;
 
 
 fn main() {
-    let file: String = read_to_string("src/main.dots")
-        .expect("Couldn't read file.");
+    let args: Vec<String> = env::args().collect();
+    dbg!(args);
 
-    let tokens: Vec<&str> = file
+    let file: String = read_to_string("src/main.dots")
+        .expect("Fatal Error: Couldn't read file.");
+
+    let binding = file
+        .replace('\n', "");
+
+    let tokens: Vec<&str> = binding
         .trim()
-        .split(";\n")
+        .split(";")
         .collect();
 
-    // Bit ugy but idk
-    let dots_eop: &str = ".;";
-    let dots_print: &str = "..";
-    let dots_var: &str = "...";
+    // A bit ugy
+    const DOTS_EOP: &str = ".";
+    const DOTS_PRINT: &str = "..";
+    const DOTS_VAR: &str = "...";
 
     // All vars are global
-    let mut dots_variables = HashMap::new();
+    let mut dots_variables: HashMap<String, String> = HashMap::new();
 
-    assert!(tokens[tokens.len() - 1].ends_with(dots_eop));
+    // There should probably be a way to check if the program ends with a .;
 
     for t in tokens {
-        // Just dupe it so we can remove the leading dots
-        let mut token = String::from(t);
+        let mut token: String = String::from(t);
 
-        println!("{}", token);
-
-        if token == dots_eop {
-            println!("Reached the end of the program.");
+        if token == DOTS_EOP {
+            println!("\nReached the end of the program.");
             return;
         }
-        else if token.starts_with(dots_var) {
-            // Tokenize: [...][name][value]
-            // Everything else can be ignored so
-            // we just don't look at whatever is after that
 
-            for _ in 0..=2 {
+        else if token.starts_with(DOTS_VAR) {
+            // Remove the ... from the token
+            for _ in 0..3 {
+                // Remove the ...
                 token.remove(0);
             }
-
-            println!("{}", token);
+            let token: String = String::from(token
+                .trim()
+            );
 
             let split_token: Vec<&str> = token
                 .split(" ")
                 .collect();
 
-            // TODO: Add some logic to accept `... name` & `...name value` as valid
-            // TODO: maybe throw an error if the syntax is invalid?
+            if split_token.len() > 2 {
+                println!("Syntax error: Expected `...name value`, got '{}'.", token);
+                return;
+            }
 
-            dots_variables.insert(split_token[1].to_string(),
-                                  split_token[2].to_string());
-
+            // Considering there's nothing to do with variables,
+            // overwrite time
+            dots_variables.insert(
+                split_token[0]
+                    // Needs to be parsed into a String because otherwise
+                    // it gets dropped
+                    .parse()
+                    .unwrap(),
+                split_token[1]
+                    .parse()
+                    .unwrap()
+            );
         }
-        else if token.starts_with(dots_print) {
-            // TODO: Slice token starting from ..!
-            // Imma go with .name. for printing variables because why not
+        else if token.starts_with(DOTS_PRINT) {
+            /*
+            Since there's nothing to do with variables I'm just not going to
+            try to implement printing them, raw printing time it is, bay bee.
+            */
 
-            for _ in 0..=1 {
+            // Remove the .. from the token
+            for _ in 0..2 {
                 token.remove(0);
             }
 
-            let split_token: Vec<&str> = token
-                .split(" ")
-                .collect();
+            // Yeag
+            print!("{}", token);
         }
     }
 }
